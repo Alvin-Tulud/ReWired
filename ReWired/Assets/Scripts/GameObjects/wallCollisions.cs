8c259f0a94d126323571838f2417d59f968a7eae
Alvin-Tulud <alvintulud@gmail.com>
2024-03-26 19:50:34 -0700

    wall collisions work

---
+++ ReWired/Assets/Scripts/GameObjects/wallCollisions.cs
@@ -0,0 +1,52 @@
+using GridObjects.Components.CompTypes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class wallCollisions : MonoBehaviour
+{
+    public Tilemap tilemap; 
+    public List<Vector3> tileWorldLocations; // Use this for initialization
+
+
+    void Start () 
+    { 
+        tileWorldLocations = new List<Vector3>(); 
+
+
+        foreach (var pos in tilemap.cellBounds.allPositionsWithin) 
+        { 
+            Vector3Int localPlace = new Vector3Int(pos.x, pos.y, pos.z); 
+            Vector3 place = tilemap.CellToWorld(localPlace); 
+
+
+            if (tilemap.HasTile(localPlace)) 
+            { 
+                tileWorldLocations.Add(place); 
+            } 
+        } 
+        print(tileWorldLocations); 
+
+
+        //for each wall we get in the tilemap set the editor tile under it to not walkable
+        foreach(Vector3 wall in tileWorldLocations)
+        {
+            Vector3 boardConversion = new Vector3(wall.x + 0.5f, wall.y + 0.5f, wall.z);
+
+            RaycastHit2D ray;
+            ray = Physics2D.Raycast(boardConversion, Vector2.right,0.1f);
+
+            Debug.Log("wall || " + wall + " || " + "boardConversion || " + boardConversion + " || " + ray.transform.name + " || " + ray.transform.position);
+
+            ray.transform.GetComponent<CanWalk>().setWalkable(false);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
