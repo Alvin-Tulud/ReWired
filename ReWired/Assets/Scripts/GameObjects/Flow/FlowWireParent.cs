89e2c5ddc7d27af4b57dd3de344726f49a0ad12c
alee84sf <alee84@sfsu.edu>
2024-03-31 03:37:05 -0700

    made flow parent files

---
+++ ReWired/Assets/Scripts/GameObjects/Flow/FlowWireParent.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowWireParent : MonoBehaviour
+{
+    /* The parent class for flow wires. 
+     * This has all the functions needed for flow wires to behave normally
+     * Every flow wire needs to inherit this, since it gives them its variables and functions.
+     * 
+     * if you're reading this, I AM FIXING IT AND WILL COME BACK LATER, trust :fire:
+     */
+
+    //TODO: I need to add:
+    // - New tag for the flow object so it knows whether colliding objects are flow wires
+
+    bool poweredState; //Whether the flow is on or off
+    bool updated; //Keeps track of whether a flow wire is updatedFalse, true when updating (so below function doesn't loop back into itself), false again when done updating
+
+    void Start()
+    {
+        updated = false;
+    }
+
+    //Updates the whole connected flow wire's state recursively.
+    public void updateWire(bool state)
+    {
+        selfUpdate(state);
+
+        //for every other flow wire touching this one:
+        //If its updated=false, updateWire it so it can update the others
+    }
+
+    //Updates THIS flow tile's state only
+    public void selfUpdate(bool state)
+    {
+        poweredState = state;
+        updated = true;
+    }
+}
