32cb0a9bdc34a8451a7b76274302c73f8b07e6fb
Gregory Arruiza <gregoryarruiza@gmail.com>
2024-04-11 18:59:36 -0700

    WireNub
    yay

--- ReWired/Assets/Scripts/GameObjects/DrawWire.cs
+++ ReWired/Assets/Scripts/GameObjects/DrawWire.cs
@@ -7,26 +7,31 @@ public class DrawWire : MonoBehaviour
     Transform otherWireNode;
     int childNodeIndex;
     LineRenderer wireLine;
+    public Color32 wireColor;
+
     // Start is called before the first frame update
     void Start()
     {
-        childNodeIndex = transform.GetSiblingIndex();
-        if (childNodeIndex == 0)
-            otherWireNode = transform.parent.GetChild(1);
-        else
-            otherWireNode = transform.parent.GetChild(0);
-
-
         wireLine = GetComponent<LineRenderer>();
+
+        wireLine.startColor = wireColor;
+        GameObject[] found = GameObject.FindGameObjectsWithTag(transform.tag);
+        foreach(GameObject WireNub in found){
+            if (wireLine.startColor == WireNub.GetComponent<LineRenderer>().startColor) {
+                otherWireNode = WireNub.transform;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
         wireLine.SetPosition(0, transform.position);
         wireLine.SetPosition(1, otherWireNode.position);
 
         //if an if else for moving turn it on and else turn it off
         //make sure it is always pointing towards the other wire so like rotate it
     }
+
+
 }
