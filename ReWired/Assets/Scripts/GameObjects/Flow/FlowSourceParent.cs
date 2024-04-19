e1ede51dcb432eb4091c082d898f0b0e3204fd23
alee84sf <alee84@sfsu.edu>
2024-04-19 03:27:06 -0700

    floooowwwww

--- ReWired/Assets/Scripts/GameObjects/Flow/FlowSourceParent.cs
+++ ReWired/Assets/Scripts/GameObjects/Flow/FlowSourceParent.cs
@@ -6,15 +6,52 @@ public abstract class FlowSourceParent : MonoBehaviour
 {
     //Flow source is buttons/etc. that power flow wires
 
-    int powered;
+    bool powered;
+    private List<GameObject> connectedWires; //List of connected flow segments, populated by OnTriggerEnter2D
 
 
-
-    // Start is called before the first frame update
-    void Start()
+    //Right before start we gotta use a circle raycast to get the list of connected flow segments
+    void Awake()
     {
-        
+
     }
 
     //Update: 1.Change this thing's power state 2.trigger connected wires' recursive update
+    //this should probably be called by collision enter/exit function
+    public void updateFlowSource(bool p)
+    {
+        int val; //1 or -1 depending on powering on or off
+        if (p) //if powering ON
+        {
+            val = 1;
+        }
+        else //if powering OFF
+        {
+            val = -1;
+        }
+
+        //change this thing's own state
+        //(should probably involve sprite/animation change)
+        apply(p);
+
+        //trigger wireUpdate on connected flow wires
+        foreach (GameObject w in connectedWires)
+        {
+            //the script of the flow wire
+            FlowWire script = w.GetComponent<FlowWire>();
+
+            //Only update the wire if it hasn't been yet
+            if (!script.getUpdated()) //if updated==false
+            {
+                script.powerUpdate(val); //recursive update the connected wire
+            }
+        }
+    }
+
+    //Applies state change based on power level (powerUpdate only updates power level but doesn't trigger effects)
+    protected virtual void apply(bool p)
+    {
+        //hello bro
+    }
+
 }
